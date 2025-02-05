1fca0bcedf0277f0db85ddd7b88a1e87f586d0e8
Naveenkumar Murugan <mndlsoft@gmail.com>
2025-02-05 12:35:56 +1100

    changes

--- changelog/reference-data/currency-rates/updates/001-update-eur-rate-rollback.sql
+++
@@ -1,5 +0,0 @@
-UPDATE currency_rates 
-SET rate = 1.1000,
-    updated_at = CURRENT_TIMESTAMP,
-    created_by = 'rollback-001'
-WHERE currency_code = 'EUR';
--- changelog/reference-data/currency-rates/updates/001-update-eur-rate.sql
+++
@@ -1,5 +0,0 @@
-UPDATE currency_rates 
-SET rate = 1.1500,
-    updated_at = CURRENT_TIMESTAMP,
-    created_by = 'business-update-001'
-WHERE currency_code = 'EUR';
